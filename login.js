01e1376ca2730130ca30455fc96579f9d40bc6d1
callmedems <[demmi.zepeda@gmail.com]>
2024-05-18 19:43:36 -0600

    agregamos el js de login

---
+++ login.js
@@ -0,0 +1,10 @@
+function openModel() {
+  console.log("HOLAA")
+  document.getElementById('md').style.display = 'flex';  //none
+  
+}
+function closeModel() {
+  console.log("HOLAA")
+  document.getElementById('md').style.display = 'none';  //none
+  
+}
